498a596e0735c92559581132d351beae00ffc28b
Jyri Ojala <jyri.ojala@web-veistamo.fi>
2022-01-09 00:40:30 +0200

    init: use mongodb from database

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
 // Popular library for server on node
 const express = require("express");
 const app = express();
-const cors = require('cors')
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+// MongoDB database access setup. 
+const mongoDbpassword = process.env.MONGOPASS;
+const url = `mongodb+srv://valmi:${mongoDbpassword}@cluster0.y3ezc.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+// Transform mongoDB results for person json - change id from obj to string, rename id property and delete __version.
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model("Person", personSchema);
 
 // Allow all origin requests to all backend express routes.
-app.use(cors())
+app.use(cors());
 
-// Add ability to show static content and js 
-app.use(express.static('build'))
+// Add ability to show static content and js
+app.use(express.static("build"));
 
 /**
- * Middleware: Transforms JSON-data in requests to JS objects, 
+ * Middleware: Transforms JSON-data in requests to JS objects,
  * so they are accessible in request.body
- *  */ 
+ *  */
 app.use(express.json());
 
 /**
  * Morgan middleware setup:
  * https://github.com/expressjs/morgan
- **/ 
+ **/
 // app.use(morgan('tiny'))
-
 // Create a new token for returning POST json string
-morgan.token('req-body', function (req, res) { 
+morgan.token("req-body", function (req, res) {
   // console.log('req', req._body);
   if (req._body) {
-    return JSON.stringify(req.body)
-  } 
-})
+    return JSON.stringify(req.body);
+  }
+});
 // Use 'tiny' configuration but add json content for post request.
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :req-body"
+  )
+);
 
 // Node native http server, not so flexible as Express.
 // const http = require('http')
@@ -41,16 +66,16 @@ let persons = [
     name: "Ada Lovelace",
     number: "1111111111",
   },
-  {
-    id: 223423,
-    name: "Bull Mentula",
-    number: "65434334",
-  },
-  {
-    id: 34235235,
-    name: "Jeremy Roenikki",
-    number: "33333333333",
-  },
+  // {
+  //   id: 223423,
+  //   name: "Bull Mentula",
+  //   number: "65434334",
+  // },
+  // {
+  //   id: 34235235,
+  //   name: "Jeremy Roenikki",
+  //   number: "33333333333",
+  // },
 ];
 
 // Node default http server version of get all endpoint:
@@ -59,8 +84,18 @@ let persons = [
 //   response.end(JSON.stringify(persons))
 // })
 
+// app.get("/api/persons", (req, res) => {
+//   res.json(persons);
+// });
+
+// Get all notes through MongoDB.
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    console.log("persons:", persons);
+    res.json(persons)
+    mongoose.connection.close();
+  });
+  
 });
 
 app.get("/", (req, res) => {
@@ -95,27 +130,26 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-// Helper function to create a next unique ID in sequence. 
+// Helper function to create a next unique ID in sequence.
 const generateId = () => {
   /**
-   * To get largest id, we create a new table with all IDs, and 
-   * then turn it into values for Math.max using ...spread syntax. 
-   * */ 
+   * To get largest id, we create a new table with all IDs, and
+   * then turn it into values for Math.max using ...spread syntax.
+   * */
   const maxId =
     persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
   // Set unique id for new item to add.
   return maxId + 1;
 };
 
-// Helper function to create an random ID. 
+// Helper function to create an random ID.
 const generateRandomId = () => {
-  
   // Create a long enough random ID so it can work as unique ID.
-  
-  const randomId = Number(String(Math.random()).substring(2,10))
+
+  const randomId = Number(String(Math.random()).substring(2, 10));
   //console.log(randomId)
   // Set unique id for new item to add.
-  return randomId
+  return randomId;
 };
 
 app.post("/api/persons", (request, response) => {
@@ -128,15 +162,15 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  // Do not accept if name already exists 
-  let isPersonAlreadyInBook = persons.find(person => {
-    return person.name === requestBody.name
-  })
+  // Do not accept if name already exists
+  let isPersonAlreadyInBook = persons.find((person) => {
+    return person.name === requestBody.name;
+  });
   //console.log('isPersonAlreadyInBook:',  isPersonAlreadyInBook)
-  if ( isPersonAlreadyInBook ) {
+  if (isPersonAlreadyInBook) {
     return response.status(400).json({
       error: "name must be unique",
-    })
+    });
   }
 
   // If we create object manually like this,
@@ -159,30 +193,28 @@ app.post("/api/persons", (request, response) => {
   //console.log('All persons after the post call:', persons)
 });
 
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   const currentDate = new Date();
-  const amountOfItems = persons.length
+  const amountOfItems = persons.length;
   //console.log('date:', currentDate)
 
-  const htmlResponse = 
-  `<p> Phonebook has info for ${amountOfItems} people</p>\
-  <p> currentDate: ${currentDate} </p>`
+  const htmlResponse = `<p> Phonebook has info for ${amountOfItems} people</p>\
+  <p> currentDate: ${currentDate} </p>`;
 
-  response.send(htmlResponse)
-})
+  response.send(htmlResponse);
+});
 
 /**
- * Middleware: we use it after the routes in code, 
+ * Middleware: we use it after the routes in code,
  * so this only affects those paths that do not have set routes.
  */
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
